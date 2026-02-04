25d3c1d59f9a436d12c06aeb7393b984a9dbd644
Alex <yinhezhu@YINHEs-MacBook-Pro.local>
2026-02-04 15:28:34 +0200

    completed exercise 3.4: Phonebook backend step 4

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.get('/info', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
     persons = persons.filter(person => person.id !== id)
-    response.status(204).send('Deleted successfully')
+    response.status(204).end()
 })
 
 const generateId =  () => {
